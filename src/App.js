f3d62e1a40b0eb9fd7fe563146525cc40470d23e
Vlad Spatariu <vspatariu@gmail.com>
2024-05-30 16:58:49 +0300

    fixed failed test because of missing learn react text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> has been changed.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on a feature branch!
+          Learn React
         </a>
       </header>
     </div>
